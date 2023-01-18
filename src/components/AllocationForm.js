d479306c7bc5e87285ac231ff6a9f44c7492ec6a
Mr Smith <john.smith_SAC_2045@outlook.com>
2023-01-18 01:22:00 -0500

    IBM Course Week 4 Assignment

--- src/components/AllocationForm.js
+++ src/components/AllocationForm.js
@@ -3,7 +3,7 @@ import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const AllocationForm = (props) => {
-    const { dispatch,remaining  } = useContext(AppContext);
+    const { dispatch,remaining,currency } = useContext(AppContext);
 
     const [name, setName] = useState('');
     const [cost, setCost] = useState('');
@@ -12,7 +12,7 @@ const AllocationForm = (props) => {
     const submitEvent = () => {
 
             if(cost > remaining) {
-                alert("The value cannot exceed remaining funds  £"+remaining);
+                alert("The value cannot exceed remaining funds  "+currency+" " +remaining);
                 setCost("");
                 return;
             }
@@ -59,7 +59,11 @@ const AllocationForm = (props) => {
                         <option defaultValue value="Add" name="Add">Add</option>
                 <option value="Reduce" name="Reduce">Reduce</option>
                   </select>
-
+                     <div style={{display:"flex",alignItems:"right", justifyContent:"center"}}>
+                        <label htmlFor="cost" style={{ marginLeft: '2rem'}}>
+                         {currency}
+                        </label>
+                    </div>
                     <input
                         required='required'
                         type='number'
--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -99,9 +99,9 @@ export const AppProvider = (props) => {
     // 4. Sets up the app state. takes a reducer, and an initial state
     const [state, dispatch] = useReducer(AppReducer, initialState);
     let remaining = 0;
-
+    let totalExpenses =0;
     if (state.expenses) {
-            const totalExpenses = state.expenses.reduce((total, item) => {
+            totalExpenses = state.expenses.reduce((total, item) => {
             return (total = total + item.cost);
         }, 0);
         remaining = state.budget - totalExpenses;
@@ -111,6 +111,7 @@ export const AppProvider = (props) => {
         <AppContext.Provider
             value={{
                 expenses: state.expenses,
+                totalExpenses,
                 budget: state.budget,
                 remaining: remaining,
                 dispatch,
